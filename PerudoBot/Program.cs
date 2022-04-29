475a0062ae3d8f1f4b2dcd56dbec5458960edeff
agoryelov <andrey.goryelov@gmail.com>
2022-04-28 23:14:42 -0700

    Use interactions framework

--- PerudoBot/Program.cs
+++ PerudoBot/Program.cs
@@ -1,198 +1,79 @@
 ﻿using Discord;
-using Discord.Commands;
+using Discord.Interactions;
 using Discord.WebSocket;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PerudoBot.Database.Data;
 using System;
 using System.IO;
-using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
-using Serilog;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Caching.Memory;
 
-namespace PerudoBot
+namespace Perudobot
 {
-    partial class Program
+    public class Program
     {
-        private static void Main(string[] args)
+        private readonly IConfiguration _configuration;
+        private readonly IServiceProvider _services;
+
+        private readonly DiscordSocketConfig _socketConfig = new()
         {
-            var p = new Program();
-            p.RunBotASync().GetAwaiter().GetResult();
-        }
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+            AlwaysDownloadUsers = true,
+        };
 
-        private DiscordSocketClient _client;
-        private CommandService _commands;
-        private IServiceProvider _services;
-        private IConfigurationRoot _configuration;
-        private ILogger<Program> _logger;
-
-        public async Task RunBotASync()
+        public Program()
         {
             _configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false)
+                .AddJsonFile("appsettings.json", optional: true)
                 .Build();
 
-            var config = new DiscordSocketConfig { MessageCacheSize = 100, LogLevel = LogSeverity.Verbose };
-            _client = new DiscordSocketClient(config);
-            _commands = new CommandService();
-
-            // Specifying the configuration for serilog
-            Log.Logger = new LoggerConfiguration()
-                            .ReadFrom.Configuration(_configuration)
-                            .Enrich.FromLogContext()
-                            .WriteTo.SQLite(_configuration.GetConnectionString("SerilogDb"))
-                            .WriteTo.Console(restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning)
-                            .MinimumLevel.Debug()
-                            .CreateLogger();
-
-            var memory = new MemoryCache(new MemoryCacheOptions());
             _services = new ServiceCollection()
-                .AddMemoryCache()
-                .AddSingleton(_client)
-                .AddSingleton(_commands)
                 .AddSingleton(_configuration)
-                .AddLogging(configure => configure.AddSerilog())
-                .AddEntityFrameworkSqlite()
-                .AddDbContext<PerudoBotDbContext>(options =>
-                    options.UseLazyLoadingProxies()
-                           .UseSqlite(_configuration.GetConnectionString("PerudoBotDb")))
+                .AddSingleton(_socketConfig)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<InteractionHandler>()
                 .BuildServiceProvider();
-
-            var db = _services.GetRequiredService<PerudoBotDbContext>();
-            db.Database.Migrate();
-
-            _logger = _services.GetRequiredService<ILogger<Program>>();
-            _logger.LogInformation("App Starting");
-
-            var token = _configuration.GetSection("DiscordToken").Value;
-            _client.Log += OnLogAsync;
-
-            await RegisterCommandsAsync();
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
-
-            await Task.Delay(-1);
         }
 
-        private Task OnLogAsync(LogMessage msg)
+        static void Main(string[] args)
         {
-            string logText = $": {msg.Exception?.ToString() ?? msg.Message}";
-            switch (msg.Severity.ToString())
-            {
-                case "Critical":
-                    {
-                        _logger.LogCritical(logText);
-                        break;
-                    }
-                case "Warning":
-                    {
-                        _logger.LogWarning(logText);
-                        break;
-                    }
-                case "Info":
-                    {
-                        _logger.LogInformation(logText);
-                        break;
-                    }
-                case "Verbose":
-                    {
-                        _logger.LogInformation(logText);
-                        break;
-                    }
-                case "Debug":
-                    {
-                        _logger.LogDebug(logText);
-                        break;
-                    }
-                case "Error":
-                    {
-                        _logger.LogError(logText);
-                        break;
-                    }
-            }
-
-
-            return Task.CompletedTask;
+            new Program().RunAsync()
+                .GetAwaiter()
+                .GetResult();
         }
 
-        public async Task RegisterCommandsAsync()
+        public async Task RunAsync()
         {
-            _client.MessageReceived += HandleCommandAsync;
-            _client.ReactionAdded += HandleReactionAddedAsync;
-            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            var client = _services.GetRequiredService<DiscordSocketClient>();
+
+            client.Log += LogAsync;
+
+            // Here we can initialize the service that will register and execute our commands
+            await _services.GetRequiredService<InteractionHandler>()
+                .InitializeAsync();
+
+            // Bot token can be provided from the Configuration object we set up earlier
+            await client.LoginAsync(TokenType.Bot, _configuration["DiscordToken"]);
+            await client.StartAsync();
+
+            // Never quit the program until manually forced to.
+            await Task.Delay(Timeout.Infinite);
         }
 
-        private async Task HandleReactionAddedAsync(Cacheable<IUserMessage, ulong> before, ISocketMessageChannel after, SocketReaction reaction)
+        private async Task LogAsync(LogMessage message)
         {
-            var message = await before.GetOrDownloadAsync();
-
-            if (message == null) return;
-            if (IsReactionMine(reaction) || !IsMessageMine(message)) return;
-
-            var context = new CommandContext(_client, message);
-            Console.WriteLine($"Handling reaction of {reaction.Emote.Name}");
-            var result = await _commands.ExecuteAsync(context, reaction.Emote.Name, _services);
+            Console.WriteLine(message.ToString());
         }
 
-        private async Task HandleCommandAsync(SocketMessage arg)
+        public static bool IsDebug()
         {
-            if (arg is not SocketUserMessage message) return;
-            var context = new SocketCommandContext(_client, message);
-
-            IResult result = null;
-
-            var prefix = _configuration.GetSection("BotCommandPrefix").Value;
-
-            var argPos = 0;
-            if (message.HasStringPrefix(prefix, ref argPos))
-            {
-                result = await _commands.ExecuteAsync(context, argPos, _services);
-            }
-            else if (message.HasStringPrefix(context.Client.CurrentUser.Mention, ref argPos))
-            {
-                result = await _commands.ExecuteAsync(context, "version", _services);
-            }
-            if (result != null)
-            {
-                if (!result.IsSuccess)
-                {
-
-                    switch (result.Error)
-                    {
-                        case CommandError.BadArgCount:
-                            //await context.Channel.SendMessageAsync("Bad argument count.");
-                            break;
-                        case CommandError.UnknownCommand:
-                            break;
-                        case CommandError.Exception:
-                            if (result is ExecuteResult execResult)
-                            {
-                                //you can now access execResult.Exception to see what happened
-                                _logger.LogError(exception: execResult.Exception, message: execResult.ErrorReason, args: null);
-                            }
-                            break;
-                        default:
-                            _logger.LogError(result.ErrorReason);
-                            break;
-                    }
-                    
-
-                }
-            }
-        }
-
-        private bool IsMessageMine(IUserMessage message)
-        {
-            return _client.CurrentUser.Id == message.Author.Id;
-        }
-
-        private bool IsReactionMine(SocketReaction reaction)
-        {
-            return _client.CurrentUser.Id == reaction.UserId;
+#if DEBUG
+            return true;
+#else
+                return false;
+#endif
         }
     }
 }
